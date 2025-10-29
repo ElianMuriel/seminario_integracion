39d957fc808ed2abd8f74873a16a67946ea9a182
elian muriel <elian_2546@hotmail.com>
2025-10-28 19:53:13 -0500

    clase 28/10/2025

--- django/billing_api/billing_api/urls.py
+++ django/billing_api/billing_api/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('api/', include('users.urls')),   # <-- todo lo de usuarios/auth/admin
+    path('api/', include('users.urls')),   # <-- todo lo de usuarios/auth/admin
     # En Parte 2 y 3 añadiremos:
     # path('api/', include('catalog.urls'))
     # path('api/', include('invoices.urls'))
